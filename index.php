98f76e1120edda6df0a9c1bb9f2542951b944c3e
JaganKumarMohanty <jaganmohanty2001@gmail.com>
2025-07-24 16:29:57 +0530

    Header added

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <!DOCTYPE html>
 <HTML>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>E&E Services</title>
 </head>
 <body>
+   <header>
+     <div class="title">
+      <h2>E&E Services<br>
+      Made with ..<span>❤</span></h2>
+       
+     </div>
+   </header>
 </body>
 </HTML
