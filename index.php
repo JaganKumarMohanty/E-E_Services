47d97597308c0ecfd5c27b96406a0798a73192d6
JaganKumarMohanty <jaganmohanty2001@gmail.com>
2025-07-24 17:03:37 +0530

    Header updated

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>E&E Services</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="index.css">
 </head>
 <body>
    <header>
      <div class="title">
-      <h2>E&E Services<br>
-      Made with ..<span>❤</span></h2>
-       
+      <h2><strong>E&E Services</strong></h2> 
      </div>
    </header>
 </body>
